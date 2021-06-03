b2bd42b706a025acfdffb37e655f4b783199cf28
IflameI <67149445+IflameI@users.noreply.github.com>
2021-06-03 10:00:43 +0300

    Version 2.0

--- sketch.js
+++ sketch.js
@@ -1,26 +1,28 @@
 let slider;
 let phase = 0;
+let zoff = 0;
+let mic;
 function setup() {
   createCanvas(600, 600);
-  slider = createSlider(0, 10, 5, 0.1);
+  slider = createSlider(0, 10, 3, 0.1);
 }
 
 function draw() {
-  background(0);
+  background(0, 25);
   translate(width / 2, height / 2);
   stroke(255);
   noFill();
   beginShape();
   let noiseMax = slider.value();
-  for (let a = 0; a < TWO_PI; a += 0.1) {
-    let xoff = map(cos(a + phase), -1, 1, 0, noiseMax);
-    let yoff = map(sin(a + phase), -1, 1, 0, noiseMax);
-    let r = map(noise(xoff, yoff), 0, 1, 100, 200);
+  for (let a = 0; a < TWO_PI; a += 0.05) {
+    let xoff = map(cos(a), -1, 1, 0, noiseMax);
+    let yoff = map(sin(a), -1, 1, 0, noiseMax);
+    let r = map(noise(xoff, yoff, zoff), 0, 1, 100, 200);
     let x = r * cos(a);
     let y = r * sin(a);
     vertex(x, y);
   }
   endShape(CLOSE);
-
-  phase += 0.1;
+  phase += 0.01;
+  zoff += 0.01;
 }
